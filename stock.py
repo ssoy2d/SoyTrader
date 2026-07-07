1fcc37a4e0472f859ae8864f89e50f29475eeca0
ssoy2d <so97072003@gmail.com>
2026-07-07 18:08:25 +0900

    Fix stock import error

--- stock.py
+++ stock.py
@@ -1,22 +1,26 @@
 import yfinance as yf
 
 def get_stock_data(ticker):
-    stock = yf.Ticker(ticker)
-    data = stock.history(period="5d")
+    try:
+        stock = yf.Ticker(ticker)
+        data = stock.history(period="5d")
 
-    if data.empty or len(data) < 2:
+        if data.empty or len(data) < 2:
+            return None
+
+        latest = data.iloc[-1]
+        previous = data.iloc[-2]
+
+        close = latest["Close"]
+        prev_close = previous["Close"]
+        change = ((close - prev_close) / prev_close) * 100
+        volume = latest["Volume"]
+
+        return {
+            "price": close,
+            "change": change,
+            "volume": volume
+        }
+
+    except Exception:
         return None
-
-    latest = data.iloc[-1]
-    previous = data.iloc[-2]
-
-    close = latest["Close"]
-    prev_close = previous["Close"]
-    change = ((close - prev_close) / prev_close) * 100
-    volume = latest["Volume"]
-
-    return {
-        "price": close,
-        "change": change,
-        "volume": volume
-    }
